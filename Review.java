880e026da65695354911d4a86381eaef095210fd
JayIsler14 <157390822+JayIsler14@users.noreply.github.com>
2024-11-19 11:41:44 -0500

    Add files via upload

---
+++ Review.java
@@ -0,0 +1,65 @@
+package Group3_CSC340.TSRP_Backend.Review;
+
+import Group3_CSC340.TSRP_Backend.student.Student;
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+
+@Entity
+@Table(name ="reviews")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int review_id;
+
+
+
+    private String  reviewText;
+
+
+
+
+
+
+
+    public Review(int review_id, String reviewText) {
+        this.review_id = review_id;
+
+        this.reviewText = reviewText;
+
+
+    }
+
+    public Review(String reviewText) {
+
+        this.reviewText = reviewText;
+
+    }
+
+    public Review() {
+
+    }
+
+    public int getR_id() {
+        return review_id;
+    }
+
+    public void setR_id(int review_id) {
+        this.review_id = review_id;
+    }
+
+
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+
+}
---
+++ ReviewController.java
@@ -0,0 +1,48 @@
+package Group3_CSC340.TSRP_Backend.Review;
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/reviews")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService Reviewservice;
+
+    @GetMapping("/all")
+    public String getAllReviews(Model model) {
+        model.addAttribute( "reviewList", Reviewservice.getAllReviews());
+        return "review-list";
+    }
+
+    @GetMapping("/{review_Id}")
+    public String getOneReview(@PathVariable int review_Id, Model model) {
+        model.addAttribute("post", Reviewservice.getReviewById(review_Id));
+        return "t_profile";
+    }
+
+    @GetMapping("/createForm")
+    public String showCreateForm(){
+
+        return "review-new";
+    }
+
+    @PostMapping("/new")
+    public String addNewAnimal(Review review) {
+        Reviewservice.saveReview(review);
+        return "redirect:/reviews/all";
+    }
+
+
+    @GetMapping("delete/{review_id}")
+    public String deleteReviewById(@PathVariable int review_id) {
+        Reviewservice.deleteReviewById(review_id);
+        return "redirect:/reviews/all";
+    }
+}
